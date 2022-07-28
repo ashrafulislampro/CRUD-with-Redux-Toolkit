26824f4ff98c0a6334621303c62df9f65ce59160
ashrafulislampro <ashrafulislam1659@gmail.com>
2022-07-28 11:29:32 +0600

    Redux toolkit project setup

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./App.css";
+function App() {
+  return (
+    <div className="App">
+      <h1>React Redux And Redux-Toolkit Tutorial Practice.</h1>
+    </div>
+  );
+}
+
+export default App;
